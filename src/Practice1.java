9c5cdd1cb90c08af2088655cd175b2e63fba594b
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-01-18 10:42:28 -0500

    Tuesday's requirement added

--- src/Practice1.java
+++ src/Practice1.java
@@ -3,4 +3,26 @@ public class Practice1 {
     //hello Tuesday
 //Whether is not so good
     //will not swim
+    //water  is not so warm
 }
+/*
+1. create a java project called git_practice3
+2. add .gitigore file with below content
+.idea
+out
+*.iml
+
+3. enable version control to create new repo for this project
+4. make your initial commit
+5. create a new class called Practice1
+6. make few changes and few commits
+7. undo your last commit
+
+## Github
+1. Create new repository in Github.com
+2. learn to make it public or private
+3. Make sure do not add anything into your newly created github repo to avoid furthur issues
+4. now back to your IntelliJ , create connection between your local repo and remote repo
+   1. go to Git menu -- Manage Remote and paste in your remote repo url
+5. now you have connection and you can push
+ */
